f84d70dd51f9f03142e911f8156cf8a1fb260e28
bullet162 <dalisayfernando4@gmail.com>
2026-05-08 14:13:57 +0800

    feat: BlazorPortfolio/Migrations/20260508061238_AddPersistentTokenToAdminUser.cs

---
+++ BlazorPortfolio/Migrations/20260508061238_AddPersistentTokenToAdminUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorPortfolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPersistentTokenToAdminUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PersistentToken",
+                table: "AdminUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PersistentTokenExpiresAt",
+                table: "AdminUsers",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PersistentToken",
+                table: "AdminUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PersistentTokenExpiresAt",
+                table: "AdminUsers");
+        }
+    }
+}
