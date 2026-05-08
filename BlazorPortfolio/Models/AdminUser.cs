3c68d3a1b6f024346672eb40c41ec5b5ed55652e
bullet162 <dalisayfernando4@gmail.com>
2026-05-08 14:13:53 +0800

    feat: BlazorPortfolio/Models/AdminUser.cs

--- BlazorPortfolio/Models/AdminUser.cs
+++ BlazorPortfolio/Models/AdminUser.cs
@@ -6,4 +6,6 @@ public class AdminUser
     public string Username { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
+    public string? PersistentToken { get; set; }
+    public DateTime? PersistentTokenExpiresAt { get; set; }
 }
