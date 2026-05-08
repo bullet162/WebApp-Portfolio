6bedc1295d7af051a3660686f700d3101f2dc591
bullet162 <dalisayfernando4@gmail.com>
2026-05-08 14:13:54 +0800

    feat: BlazorPortfolio/Program.cs

--- BlazorPortfolio/Program.cs
+++ BlazorPortfolio/Program.cs
@@ -13,13 +13,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Fix for Render/Linux "inotify instances has been reached" error
 // Disables file watching for appsettings.json
-builder.Host.ConfigureAppConfiguration((context, config) =>
+foreach (var source in builder.Configuration.Sources.OfType<Microsoft.Extensions.Configuration.Json.JsonConfigurationSource>())
 {
-    foreach (var source in config.Sources.OfType<Microsoft.Extensions.Configuration.Json.JsonConfigurationSource>())
-    {
-        source.ReloadOnChange = false;
-    }
-});
+    source.ReloadOnChange = false;
+}
 var allowedFrameAncestors = builder.Configuration
     .GetSection("Embedding:AllowedFrameAncestors")
     .Get<string[]>()?
